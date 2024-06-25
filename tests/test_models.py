dda684528f3956b3ea887ec94f0edf2ca4ac0f74
AJB2307 <allybaldelli@Allys-MacBook-Air-2.local>
2024-06-25 16:43:03 -0400

    added git workflow items

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,10 +3,11 @@ import pytest
 import sys
 import os
 
-#TODO: fix this import 
-loc = '/Users/allybaldelli/Desktop/AMNH-stuff/Doublet-work/Doublet-Quantifiers'
+# TODO: fix this import
+loc = "/Users/allybaldelli/Desktop/AMNH-stuff/Doublet-work/Doublet-Quantifiers"
 sys.path.append(os.path.abspath(loc))
 from Doublet_Quantifiers.models import *
+
 
 def test_gaussian():
     """Test the gaussian function outputs the correct values."""
@@ -15,9 +16,11 @@ def test_gaussian():
     sigma = 1
     mu = 0
     output_array = gaussian(x, A, sigma, mu)
-    expected_array = np.array([1.00000000e+00, 1.35335283e-01, 3.35462628e-04, 1.52299797e-08,
-       1.26641655e-14])
+    expected_array = np.array(
+        [1.00000000e00, 1.35335283e-01, 3.35462628e-04, 1.52299797e-08, 1.26641655e-14]
+    )
     np.testing.assert_allclose(output_array, expected_array)
+
 
 # def test_voigt():
 #     """Test the voigt function outputs the correct values."""
